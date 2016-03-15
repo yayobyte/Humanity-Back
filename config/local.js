b72d0dc003fec6b21977ae57f5bc73a946508ddc
Cristian Gutiérrez <yayobyte@gmail.com>
2016-03-15 16:12:29 -0500

    Openshift Modifications

--- config/local.js
+++ config/local.js
@@ -80,6 +80,8 @@ module.exports = {
    * 'development' environment.                                              *
    ***************************************************************************/
 
-   // environment: process.env.NODE_ENV || 'development'
+  host: process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1",
+  port: process.env.OPENSHIFT_NODEJS_PORT || 8080,
+  environment: process.env.NODE_ENV || 'development'
 
 };
