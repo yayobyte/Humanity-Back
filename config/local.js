5436035c8d164efd6b99797ed8e2c98f932ed684
Cristian Gutiérrez <yayobyte@gmail.com>
2016-03-17 14:04:23 -0500

    Changed dev port

--- config/local.js
+++ config/local.js
@@ -81,7 +81,7 @@ module.exports = {
    ***************************************************************************/
 
   host: process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1",
-  port: process.env.OPENSHIFT_NODEJS_PORT || 8080,
+  port: process.env.OPENSHIFT_NODEJS_PORT || 1337,
   environment: process.env.NODE_ENV || 'development'
 
 };
